d706b46cf5735fb9e0975b9d77b37a26627aace5
kyru odioso <kanu_nirvana@hotmail.com>
2022-06-13 01:50:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/* Hacer un programa que solicite el ingreso de dos números y luego calcular:
+
+La resta si el primero es mayor que el segundo.
+La suma si son iguales.
+El producto si el primero es menor.
+Se deberá emitir un cartel por pantalla con el resultado correspondiente.
+*/
+
+int n1, n2, resultado;
+
+Console.WriteLine("ingrese dos numeros");
+n1= int.Parse(Console.ReadLine());
+n2= int.Parse(Console.ReadLine());
+
+if(n1 > n2){
+   resultado= n1 - n2;
+}else if( n1 == n2){
+    resultado = n1 + n2;
+}else{
+    resultado= n1 * n2;
+}
+
+Console.WriteLine(resultado);
